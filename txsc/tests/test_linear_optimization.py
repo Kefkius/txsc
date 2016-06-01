9c5d3ead4c1f5f6a0dbec688410abc9340030e15
Kefkius <kefkius@mail.com>
2016-06-01 03:05:30 -0400

    Refactor linear optimization tests

--- txsc/tests/test_linear_optimization.py
+++ txsc/tests/test_linear_optimization.py
@@ -9,7 +9,8 @@ class BaseOptimizationTest(unittest.TestCase):
     def setUp(self):
         self.optimizer = LinearOptimizer()
 
-    def _do_test(self, expected, script):
+    def _do_test(self, expected, ops_list):
+        script = LInstructions(ops_list)
         original = str(script)
         self.optimizer.optimize(script, SymbolTable())
         expected = str(expected.split(' '))
@@ -17,119 +18,117 @@ class BaseOptimizationTest(unittest.TestCase):
 
 class OptimizationTest(BaseOptimizationTest):
     def test_repeated_ops(self):
-        script = LInstructions([types.Five(), types.Five(), types.Five(),
-                types.Drop(), types.Drop()])
+        script = [types.Five(), types.Five(), types.Five(), types.Drop(), types.Drop()]
         self._do_test('OP_5 OP_5 OP_5 OP_2DROP', script)
 
     def test_optimize_stack_ops(self):
-        script = LInstructions([types.Five(), types.One(), types.Pick()])
+        script = [types.Five(), types.One(), types.Pick()]
         self._do_test('OP_5 OP_OVER', script)
 
-        script = LInstructions([types.Five(), types.One(), types.Roll(), types.Drop()])
+        script = [types.Five(), types.One(), types.Roll(), types.Drop()]
         self._do_test('OP_5 OP_NIP', script)
 
-        script = LInstructions([types.Zero(), types.Pick()])
+        script = [types.Zero(), types.Pick()]
         self._do_test('OP_DUP', script)
 
-        script = LInstructions([types.Five(), types.Zero(), types.Roll()])
+        script = [types.Five(), types.Zero(), types.Roll()]
         self._do_test('OP_5', script)
 
-        script = LInstructions([types.Five(), types.Six(), types.One(), types.Roll(), types.One(), types.Roll()])
+        script = [types.Five(), types.Six(), types.One(), types.Roll(), types.One(), types.Roll()]
         self._do_test('OP_5 OP_6', script)
 
     def test_shortcut_ops(self):
         for script in [
-            LInstructions([types.Five(), types.One(), types.Add()]),
-            LInstructions([types.One(), types.Five(), types.Add()]),
+            [types.Five(), types.One(), types.Add()],
+            [types.One(), types.Five(), types.Add()],
         ]:
             self._do_test('OP_5 OP_1ADD', script)
 
-        self._do_test('OP_1ADD', LInstructions([types.Assumption('testItem', 0), types.One(), types.Add()]))
+        self._do_test('OP_1ADD', [types.Assumption('testItem', 0), types.One(), types.Add()])
 
-        script = LInstructions([types.Five(), types.One(), types.Sub()])
+        script = [types.Five(), types.One(), types.Sub()]
         self._do_test('OP_5 OP_1SUB', script)
 
         for script in [
-            LInstructions([types.Five(), types.Two(), types.Mul()]),
-            LInstructions([types.Two(), types.Five(), types.Mul()]),
+            [types.Five(), types.Two(), types.Mul()],
+            [types.Two(), types.Five(), types.Mul()],
         ]:
             self._do_test('OP_5 OP_2MUL', script)
 
-        self._do_test('OP_2MUL', LInstructions([types.Assumption('testItem', 0), types.Two(), types.Mul()]))
+        self._do_test('OP_2MUL', [types.Assumption('testItem', 0), types.Two(), types.Mul()])
 
-        script = LInstructions([types.Five(), types.Two(), types.Div()])
+        script = [types.Five(), types.Two(), types.Div()]
         self._do_test('OP_5 OP_2DIV', script)
 
-        script = LInstructions([types.Five(), types.One(), types.Negate()])
+        script = [types.Five(), types.One(), types.Negate()]
         self._do_test('OP_5 OP_1NEGATE', script)
 
     def test_null_ops(self):
         for script in [
-            LInstructions([types.Five(), types.Zero(), types.Add()]),
-            LInstructions([types.Zero(), types.Five(), types.Add()]),
-            LInstructions([types.Five(), types.Zero(), types.Sub()]),
+            [types.Five(), types.Zero(), types.Add()],
+            [types.Zero(), types.Five(), types.Add()],
+            [types.Five(), types.Zero(), types.Sub()],
         ]:
             self._do_test('OP_5', script)
 
 
     def test_merge_op_and_verify(self):
-        script = LInstructions([types.Five(), types.Five(), types.Equal(), types.Verify()])
+        script = [types.Five(), types.Five(), types.Equal(), types.Verify()]
         self._do_test('OP_5 OP_5 OP_EQUALVERIFY', script)
 
-        script = LInstructions([types.Five(), types.Five(), types.NumEqual(), types.Verify()])
+        script = [types.Five(), types.Five(), types.NumEqual(), types.Verify()]
         self._do_test('OP_5 OP_5 OP_NUMEQUALVERIFY', script)
 
     def test_optimize_dup_and_checksig(self):
-        script = LInstructions([types.Five(), types.Dup(), types.Six(), types.CheckSig()])
+        script = [types.Five(), types.Dup(), types.Six(), types.CheckSig()]
         self._do_test('OP_5 OP_6 OP_CHECKSIG', script)
 
     def test_optimize_hashes(self):
-        script = LInstructions([types.Five(), types.Sha256(), types.Sha256()])
+        script = [types.Five(), types.Sha256(), types.Sha256()]
         self._do_test('OP_5 OP_HASH256', script)
 
-        script = LInstructions([types.Five(), types.Sha256(), types.RipeMD160()])
+        script = [types.Five(), types.Sha256(), types.RipeMD160()]
         self._do_test('OP_5 OP_HASH160', script)
 
     def test_arithmetic_ops(self):
-        script = LInstructions([types.Five(), types.Five(), types.Equal(), types.Not()])
+        script = [types.Five(), types.Five(), types.Equal(), types.Not()]
         self._do_test('OP_5 OP_5 OP_NUMNOTEQUAL', script)
 
-        script = LInstructions([types.Five(), types.Push(b'\x01\x02\x03\x04'), types.Equal(), types.Not()])
+        script = [types.Five(), types.Push(b'\x01\x02\x03\x04'), types.Equal(), types.Not()]
         self._do_test('OP_5 01020304 OP_NUMNOTEQUAL', script)
 
         # A value longer than 4 bytes won't be optimized this way.
-        script = LInstructions([types.Five(), types.Push(b'\x01\x02\x03\x04\x05'), types.Equal(), types.Not()])
+        script = [types.Five(), types.Push(b'\x01\x02\x03\x04\x05'), types.Equal(), types.Not()]
         self._do_test('OP_5 0102030405 OP_EQUAL OP_NOT', script)
 
     def test_multiple_optimization_occurrences(self):
-        script = LInstructions([types.Five(), types.Five(), types.Equal(), types.Verify(),
-                               types.Five(), types.Five(), types.Equal(), types.Verify()])
+        script = [types.Five(), types.Five(), types.Equal(), types.Verify(), types.Five(), types.Five(), types.Equal(), types.Verify()]
         self._do_test('OP_5 OP_5 OP_EQUALVERIFY OP_5 OP_5 OP_EQUALVERIFY', script)
 
     def test_promote_return(self):
         for script in [
-            LInstructions([types.Five(), types.Return()]),
-            LInstructions([types.Return(), types.Five(), types.Return()]),
+            [types.Five(), types.Return()],
+            [types.Return(), types.Five(), types.Return()],
         ]:
             self._do_test('OP_RETURN OP_5', script)
 
     def test_convert_to_small_int(self):
-        script = LInstructions([types.Push(b'\x05')])
+        script = [types.Push(b'\x05')]
         self._do_test('OP_5', script)
 
 class InlineTest(BaseOptimizationTest):
     def test_implicit_assume(self):
-        script = LInstructions([types.Assumption('testItem', 0), types.Five(), types.Add()])
+        script = [types.Assumption('testItem', 0), types.Five(), types.Add()]
         self._do_test('OP_5 OP_ADD', script)
 
     def test_assume_to_pick_or_roll(self):
-        script = LInstructions([types.Five(), types.Assumption('testItem', 0), types.Add()])
+        script = [types.Five(), types.Assumption('testItem', 0), types.Add()]
         self._do_test('OP_5 OP_ADD', script)
 
-        script = LInstructions([types.Five(), types.Five(), types.Assumption('testItem', 0), types.Add()])
+        script = [types.Five(), types.Five(), types.Assumption('testItem', 0), types.Add()]
         self._do_test('OP_5 OP_5 OP_2 OP_ROLL OP_ADD', script)
 
-        script = LInstructions([types.Five(), types.Five(), types.Assumption('testItem', 0), types.Add(), types.Assumption('testItem', 0)])
+        script = [types.Five(), types.Five(), types.Assumption('testItem', 0), types.Add(), types.Assumption('testItem', 0)]
         self._do_test('OP_5 OP_5 OP_2 OP_PICK OP_ADD OP_2 OP_ROLL', script)
 
 class UnusedAssumptionsTest(BaseOptimizationTest):
@@ -137,7 +136,8 @@ class UnusedAssumptionsTest(BaseOptimizationTest):
         super(UnusedAssumptionsTest, self).setUp()
         self.symbol_table = SymbolTable()
 
-    def _do_test(self, expected, script):
+    def _do_test(self, expected, ops_list):
+        script = LInstructions(ops_list)
         original = str(script)
         self.optimizer.optimize(script, self.symbol_table)
         expected = str(expected.split(' '))
@@ -145,25 +145,25 @@ class UnusedAssumptionsTest(BaseOptimizationTest):
 
     def test_unused_top_assumption(self):
         self.symbol_table.add_stack_assumptions(['a', 'b', 'c'])
-        script = LInstructions([types.Assumption('a', 2), types.Assumption('b', 1), types.Add()])
+        script = [types.Assumption('a', 2), types.Assumption('b', 1), types.Add()]
         self._do_test('OP_DROP OP_ADD', script)
 
     def test_unused_assumptions(self):
         self.symbol_table.add_stack_assumptions(['a', 'b', 'c', 'd'])
-        script = LInstructions([types.Assumption('a', 3), types.Assumption('c', 1), types.Add()])
+        script = [types.Assumption('a', 3), types.Assumption('c', 1), types.Add()]
         self._do_test('OP_2 OP_ROLL OP_2DROP OP_ADD', script)
 
     def test_consecutive_top_item_drops(self):
         self.symbol_table.add_stack_assumptions(['a', 'b', 'c', 'd'])
 
-        script = LInstructions([types.Assumption('a', 3)])
+        script = [types.Assumption('a', 3)]
         self._do_test('OP_2DROP OP_DROP', script)
 
-        script = LInstructions([types.Assumption('b', 2)])
+        script = [types.Assumption('b', 2)]
         self._do_test('OP_2DROP OP_NIP', script)
 
-        script = LInstructions([types.Assumption('c', 1)])
+        script = [types.Assumption('c', 1)]
         self._do_test('OP_3 OP_ROLL OP_DROP OP_2 OP_ROLL OP_2DROP', script)
 
-        script = LInstructions([types.Assumption('d', 0)])
+        script = [types.Assumption('d', 0)]
         self._do_test('OP_3 OP_ROLL OP_DROP OP_2 OP_ROLL OP_DROP OP_NIP', script)
